154e81d2248fb1c40b67a0650bf785f5d87610aa
Madeline Bernstein <mbernstein96@gmail.com>
2018-07-17 16:49:59 -0400

    one hot

--- networks/networkcore.py
+++ networks/networkcore.py
@@ -218,7 +218,7 @@ class networkcore(object):
                 if isinstance(inputs[key], dict):
                     for secondard_key in inputs[key].keys():
                         fd.update({self._input[key][secondard_key] : inputs[key][secondard_key]})
-                else:
+                elif key != 'one_hot':
                     fd.update({self._input[key] : inputs[key]})
 
         # for key in fd:
